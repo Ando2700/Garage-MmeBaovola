a71db789824b84350f441a4de30db942ca675ea2
unknown <andomahefa534@gmail.com>
2023-01-10 16:36:54 +0100

    Ad

---
+++ src/java/modele/Candidat.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author ITU
+ */
+public class Candidat {
+    int id;
+    String nom;
+    String prenom;
+    Date dateNaissance;
+    String specialite;
+    Sexe sexe;
+    TypeNiveau niveau;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getSpecialite() {
+        return specialite;
+    }
+
+    public void setSpecialite(String specialite) {
+        this.specialite = specialite;
+    }
+
+    public Sexe getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(Sexe sexe) {
+        this.sexe = sexe;
+    }
+
+    public TypeNiveau getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(TypeNiveau niveau) {
+        this.niveau = niveau;
+    }
+    
+    
+}
